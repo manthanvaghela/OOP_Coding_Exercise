26e0f4d1ec5ba1bd028f1d674f642cbdb48a0c7d
manthanvaghela <httpsmanthan11@yahoo.com>
2026-02-27 16:54:21 +0530

    Add files via upload

---
+++ Set - 1/Practical 1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Practical01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter distance in meters: ");
+        double meters = scanner.nextDouble();
+
+        double feet = meters * 3.28084;
+
+        System.out.printf("%.2f meters is equal to %.2f feet.", meters, feet);
+
+        scanner.close();
+    }
+}
---
+++ Set - 1/Practical 4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Practical04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter weight in pounds: ");
+        double pounds = scanner.nextDouble();
+
+        System.out.print("Enter height in inches: ");
+        double inches = scanner.nextDouble();
+
+        double kgs = pounds * 0.45359237;
+        double meters = inches * 0.0254;
+
+        double bmi = kgs / (meters * meters);
+
+        System.out.printf("Body Mass Index is %.4f%n", bmi);
+
+        scanner.close();
+    }
+}
